9a41ecad37071ebd80beb9c694a57328124fd19c
이준호 <>
2024-09-25 22:33:13 +0900

    스프레드 연산자 이후 계속 ㄱㄱ
    ㅇㅇ

--- JavaScript 코드/부록3. 문제 모음/30. Spread 연산자와 default, rest 파라미터 관련 문제들/02 default parameter 관련 문제들.js
+++ JavaScript 코드/부록3. 문제 모음/30. Spread 연산자와 default, rest 파라미터 관련 문제들/02 default parameter 관련 문제들.js
@@ -10,7 +10,7 @@ function 함수(a = 5, b = a * 2 ){
 
 // Q2. default paramter 문제2, 대괄호가 가득한 위 코드의 출력 결과는?
 //  -> 15이 출력
-//      -> undefined는 값이 안 들어간 것으로 치부되어, default paramter대로 함수가 실행됨
+//      -> (중요) JS에서 parameter변수에 undefined이 들어간다면? 값이 안 들어간 것으로 치부 = default paramter대로 함수가 실행됨
 function 함수(a = 5, b = a * 2 ){
   console.log(a + b);
 }
